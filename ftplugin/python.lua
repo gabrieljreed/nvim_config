e59a87ad7eda90b94abd3e61782426a60b50a7e6
gabrieljreed <gabrieljreed@gmail.com>
2024-11-03 21:30:34 -0800

    init commit

---
+++ ftplugin/python.lua
@@ -0,0 +1,5 @@
+-- Python specific settings
+vim.opt.cc = "120"
+vim.opt.foldlevel = 20
+vim.opt.foldmethod = "indent"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- Use treesitter folds
---
+++ init.lua
@@ -0,0 +1,37 @@
+local config_path = vim.fn.stdpath("config")
+-- vim.cmd("source " .. config_path .. "/keybindings.vim")
+-- vim.cmd("source " .. config_path .. "/settings.vim")
+
+-- Bootstrap lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set map leader
+vim.g.mapleader = " "
+
+require("lazy").setup("plugins", {
+   change_detection = {
+     enabled = true,  -- automatically check for plugin updates
+     notify = false,  -- don't notify when plugins are updated
+   }
+})
+
+require("core.keymaps")
+
+local opt = vim.opt
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+vim.bo.softtabstop = 4
+
