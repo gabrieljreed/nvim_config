935650524fd28035dc5704a22a7fd3a8b1e18906
gabrieljreed <gabrieljreed@gmail.com>
2024-11-11 22:49:24 -0800

    Fix tab behavior when completing

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -49,26 +49,30 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         },
-	-- Tab through suggestions or when a snippet is active, tab to the next argument
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-	-- Tab backwards through suggestions or when a snippet is active, tab to the next argument
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
+        ['<Tab>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+	-- -- Tab through suggestions or when a snippet is active, tab to the next argument
+        -- ['<Tab>'] = cmp.mapping(function(fallback)
+          -- if cmp.visible() then
+            -- cmp.select_next_item()
+          -- elseif luasnip.expand_or_locally_jumpable() then
+            -- luasnip.expand_or_jump()
+          -- else
+            -- fallback()
+          -- end
+        -- end, { 'i', 's' }),
+	-- -- Tab backwards through suggestions or when a snippet is active, tab to the next argument
+        -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+          -- if cmp.visible() then
+            -- cmp.select_prev_item()
+          -- elseif luasnip.locally_jumpable(-1) then
+            -- luasnip.jump(-1)
+          -- else
+            -- fallback()
+          -- end
+        -- end, { 'i', 's' }),
       },
       sources = cmp.config.sources({
         { name = "nvim_lsp" }, -- lsp 
