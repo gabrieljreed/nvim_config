1e0fd0d65d11b213fcce9a58cd5241251b91b4fe
Gabriel Reed <gabrieljreed@gmail.com>
2024-11-12 16:48:43 -0800

    Update cmp and illuminate plugin settings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -45,10 +45,23 @@ return {
         ['<C-b>'] = cmp.mapping.scroll_docs(-4), -- scroll backward
         ['<C-f>'] = cmp.mapping.scroll_docs(4), -- scroll forward
         ['<C-Space>'] = cmp.mapping.complete {}, -- show completion suggestions
-        ['<CR>'] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        },
+        -- ['<CR>'] = cmp.mapping.confirm {
+        --   behavior = cmp.ConfirmBehavior.Replace,
+        --   select = true,
+        -- },
+        ['<CR>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if luasnip.expandable() then
+              luasnip.expand()
+            else
+              cmp.confirm({
+                select = true,
+              })
+            end
+          else
+            fallback()
+          end
+        end),
         ['<Tab>'] = cmp.mapping.confirm {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
--- lua/plugins/vim-illuminate.lua
+++ lua/plugins/vim-illuminate.lua
@@ -1,3 +1,15 @@
+-- Highlight words under cursor
 return {
   "RRethy/vim-illuminate",
+  config = function ()
+    require("illuminate").configure({
+      providers = {
+        "lsp",
+        "treesitter",
+        "regex",
+      },
+      under_cursor = true,
+      min_count_to_highlight = 1,
+    })
+  end
 }
