dcacd2427155a829726aecadea53ae415413c1ea
Gabriel Reed <gabrieljreed@gmail.com>
2024-11-18 09:42:13 -0800

    Add keybinding to toggle git blame

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,6 +9,9 @@ keymap.set("n", "<leader>to", ":tabonly<CR>", { desc = "Close other tabs ([t]ab[
 -- Toggle Nerd Tree
 keymap.set("n", "<leader>ee", ":NERDTreeToggle<CR>", { desc = "Toggle NERD Tree" })
 
+-- Toggle Git Blame
+vim.keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>", { desc = "Toggle [G]it [B]lame" } )
+
 keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", {})
 keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", {})
 
