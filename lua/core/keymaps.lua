04ca498caa41d7bc4f11884295f494ceb2796198
Gabriel Reed <gabrieljreed@gmail.com>
2024-11-18 09:58:06 -0800

    Update git blame keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,7 +10,9 @@ keymap.set("n", "<leader>to", ":tabonly<CR>", { desc = "Close other tabs ([t]ab[
 keymap.set("n", "<leader>ee", ":NERDTreeToggle<CR>", { desc = "Toggle NERD Tree" })
 
 -- Toggle Git Blame
-vim.keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>", { desc = "Toggle [G]it [B]lame" } )
+vim.keymap.set("n", "<leader>gbt", ":GitBlameToggle<CR>", { desc = "[G]it [B]lame [T]oggle" } )
+vim.keymap.set("n", "<leader>gbo", ":GitBlameCopySHA<CR>", { desc = "[G]it [B]lame [O]pen URL" } )
+vim.keymap.set("n", "<leader>gby", ":GitBlameCopySHA<CR>", { desc = "[G]it [B]lame [Y]ank SHA" } )
 
 keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", {})
 keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", {})
