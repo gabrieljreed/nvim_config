66e4395abfa7641d0126076ab6fd3a42a5427c32
Gabriel Reed <gabrieljreed@gmail.com>
2024-11-22 12:38:21 -0800

    Flesh out catppuccin config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,4 +3,21 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    config = function ()
+      local configs = require("catppuccin")
+      configs.setup({
+        flavour = "mocha",
+        dim_inactive = {
+          enabled = true,
+          shade = "dark",
+          percentage = 0.15,
+        },
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          treesitter = true,
+        }
+      })
+    end
 }
