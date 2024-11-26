d9ea8b1c84fcccce53916cf40c70d4f086f68286
Gabriel Reed <gabrieljreed@gmail.com>
2024-11-26 08:50:34 -0800

    Add command to clear shada files on Windows

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -26,10 +26,37 @@ function _G.set_terminal_keymaps()
 end
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
+-- Lazygit toggle
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
 function _lazygit_toggle()
   lazygit:toggle()
 end
 vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+
+-- Clear Shada cache
+vim.api.nvim_create_user_command("ClearShada", function ()
+    local shada_path = vim.fn.expand(vim.fn.stdpath("data") .. "/shada")
+    local files = vim.fn.glob(shada_path .. "/*", false, true)
+    local all_success = 0
+    for _, file in ipairs(files) do
+      local file_name = vim.fn.fnamemodify(file, ":t")
+      if file_name == "main.shada" then 
+        -- Skip main shada file
+        goto continue
+      end
+      local success = vim.fn.delete(file)
+      all_success = all_success + success
+      if success ~= 0 then
+        vim.notify("Couldn't delete file '" .. file_name .. "'", vim.log.levels.WARN)
+      end
+      ::continue::
+    end
+    if all_success == 0 then
+      vim.print("Successfully cleared all .tmp shada files")
+    end
+  end,
+  { desc = "Clear all .tmp shada files" }
+)
+vim.keymap.set("n", "<leader>cs", ":ClearShada<CR>", { desc = "Clear all .tmp shada files" })
 
