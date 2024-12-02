2cc7c59a31cbf07124c1ff2c065ad036e658b0c2
Gabriel Reed <gabrieljreed@gmail.com>
2024-12-02 10:14:12 -0800

    Fix powershell settings for ToggleTerm

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -5,7 +5,7 @@ return {
     require("toggleterm").setup{
     }
     local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
     function _lazygit_toggle()
       lazygit:toggle()
     end
