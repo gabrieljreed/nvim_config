d7315259cf4e4afbb08256ba3a795ca16423803e
gabrieljreed <gabrieljreed@gmail.com>
2024-12-03 20:58:40 -0800

    Add lua file keymaps

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -4,3 +4,8 @@ opt.tabstop = 2
 opt.shiftwidth = 2
 vim.bo.softtabstop = 2
 
+-- Keymaps
+vim.keymap.set("n", "<leader>x", "<cmd>source %<CR>", { desc = "Source current file (lua)" })
+vim.keymap.set("n", "<leader>X", ":.lua<CR>", { desc = "Source current line (lua)" })
+vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = "Execute selected text (lua)" })
+
