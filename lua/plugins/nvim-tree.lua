b277e2609f4d5430708848a105ee7ebf9dce3403
gabrieljreed <gabrieljreed@gmail.com>
2024-12-13 23:27:10 -0800

    Add floating preview to nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,6 +4,7 @@ return {
   lazy = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    "b0o/nvim-tree-preview.lua",
   },
   config = function ()
     require("nvim-tree").setup {
@@ -15,7 +16,36 @@ return {
         custom = {
           "^.git$",
         }
-      }
+      },
+
+      on_attach = function (bufnr)
+        local api = require("nvim-tree.api")
+        api.config.mappings.default_on_attach(bufnr)
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        local preview = require("nvim-tree-preview")
+
+        vim.keymap.set("n", "P", preview.watch, opts "Preview (Watch)")
+        vim.keymap.set("n", "<Esc>", preview.unwatch, opts "Close Preview/Unwatch")
+        vim.keymap.set("n", "<C-f>", function() return preview.scroll(4) end, opts "Scroll Down")
+        vim.keymap.set("n", "<C-b>", function() return preview.scroll(-4) end, opts "Scroll Up")
+
+        vim.keymap.set("n", "<Tab>", function ()
+         local ok, node = pcall(api.tree.get_node_under_cursor) 
+          if ok and node then
+            if node.type == "directory" then
+              api.node.open.edit()
+            else
+              preview.node(node, { toggle_focus = true })
+            end
+          end
+        end, opts "Preview")
+
+      end,
+
     }
   end
 }
