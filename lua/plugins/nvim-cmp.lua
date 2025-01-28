2d39c4078f5b63dc248fe83feed5191ceeaaacb9
Gabriel Reed <gabrieljreed@gmail.com>
2025-01-28 10:09:44 -0800

    Add custom snippets

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,33 +1,26 @@
 -- Auto-completion / Snippets
 return {
-  -- https://github.com/hrsh7th/nvim-cmp
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
     -- Snippet engine & associated nvim-cmp source
-    -- https://github.com/L3MON4D3/LuaSnip
     'L3MON4D3/LuaSnip',
-    -- https://github.com/saadparwaiz1/cmp_luasnip
     'saadparwaiz1/cmp_luasnip',
 
     -- LSP completion capabilities
-    -- https://github.com/hrsh7th/cmp-nvim-lsp
     'hrsh7th/cmp-nvim-lsp',
 
     -- Additional user-friendly snippets
-    -- https://github.com/rafamadriz/friendly-snippets
     'rafamadriz/friendly-snippets',
-    -- https://github.com/hrsh7th/cmp-buffer
     'hrsh7th/cmp-buffer',
-    -- https://github.com/hrsh7th/cmp-path
     'hrsh7th/cmp-path',
-    -- https://github.com/hrsh7th/cmp-cmdline
     'hrsh7th/cmp-cmdline',
   },
   config = function()
     local cmp = require('cmp')
     local luasnip = require('luasnip')
     require('luasnip.loaders.from_vscode').lazy_load()
+    require("luasnip.loaders.from_snipmate").lazy_load {paths = vim.fn.stdpath "config" .. "/snippets"}
     luasnip.config.setup({})
 
     cmp.setup({
