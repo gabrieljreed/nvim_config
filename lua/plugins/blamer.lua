f3e9f5c6ca83dc60758feea5868c782f5bc861a2
Gabriel Reed <gabrieljreed@gmail.com>
2025-02-07 09:26:55 -0800

    Enable git blame plugins based on OS

--- lua/plugins/blamer.lua
+++ lua/plugins/blamer.lua
@@ -1,3 +1,8 @@
+-- Inline git blame (Windows only for now)
+
+local os_name = vim.loop.os_uname().sysname
+
 return {
   "TimothyGCY/blamer.nvim",
+  cond = os_name == "Windows_NT",
 }
--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -1,5 +1,9 @@
+-- Inline git blame (Linux only for now)
+
+local os_name = vim.loop.os_uname().sysname
+
 return {
   "f-person/git-blame.nvim",
   event = "VeryLazy",
-  enabled = false,
+  cond = os_name ~= "Windows_NT",
 }
