51ccaa5751d2a7d3c527cda8bcc9879def1ac72b
Gabriel Reed <gabrieljreed@gmail.com>
2025-09-11 09:33:35 -0700

    Only enable obsidian plugin if in an obsidian vault, update spell dict

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,6 +1,11 @@
+local cwd = vim.fn.getcwd()
+cwd = string.lower(cwd)
+local in_obsidian = string.find(cwd, "obsidian")
+
 return {
   "epwalsh/obsidian.nvim",
   version = "*",
+  cond = in_obsidian,
   lazy = true,
   ft = "markdown",
   dependencies = {
