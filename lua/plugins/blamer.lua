445ce5870ac28b951d804ba4e37c0f5b5edf6c1d
Gabriel Reed <gabrieljreed@gmail.com>
2026-07-15 09:53:14 -0700

    Performance pass on blamer and vim-illuminate

--- lua/plugins/blamer.lua
+++ lua/plugins/blamer.lua
@@ -4,5 +4,13 @@ local os_name = vim.loop.os_uname().sysname
 
 return {
   "TimothyGCY/blamer.nvim",
+  cmd = {
+    "BlamerShow",
+    "BlamerHide",
+    "BlamerToggle",
+  },
+  keys = {
+    { "<leader>gbi", "<cmd>BlamerToggle<CR>", desc = "[G]it [B]lame [I]nline toggle" },
+  },
   cond = os_name == "Windows_NT",
 }
--- lua/plugins/vim-illuminate.lua
+++ lua/plugins/vim-illuminate.lua
@@ -1,7 +1,19 @@
 -- Highlight words under cursor
 return {
   "RRethy/vim-illuminate",
-  event = "VimEnter",
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = {
+    "IlluminatePause",
+    "IlluminateResume",
+    "IlluminateToggle",
+    "IlluminatePauseBuf",
+    "IlluminateResumeBuf",
+    "IlluminateToggleBuf",
+    "IlluminateDebug",
+    "IlluminationDisable",
+    "IlluminationEnable",
+    "IlluminationToggle",
+  },
   config = function ()
     require("illuminate").configure({
       providers = {
@@ -9,8 +21,14 @@ return {
         "treesitter",
         "regex",
       },
+      filetypes_denylist = {
+        "NvimTree",
+        "TelescopePrompt",
+        "lazy",
+        "mason",
+      },
       under_cursor = true,
       min_count_to_highlight = 1,
     })
-  end
+  end,
 }
